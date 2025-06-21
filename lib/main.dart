ec26d4ba92c3584055dcec4bf032d30b6e9b5c77
ShohidulProgrammer <shoikat.cse@gmail.com>
2025-06-21 23:26:41 +0600

    generated material icons mapper and material icons data mapper

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'src/views/my_app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
---
+++ lib/src/icon_builder/icon_data_generator.dart
@@ -0,0 +1,7 @@
+export '../icon_builder/builder.dart';
+export '../icon_builder/generator/material_icons_mapper_generator.dart';
+export '../icon_builder/utils/material_icon_parser.dart';
+export '../icon_builder/utils/material_icon_parser_impl.dart';
+export '../icon_builder/utils/writer/icon_data_reference_writer.dart';
+export '../icon_builder/utils/writer/icon_mapper_writer.dart';
+export '../icon_builder/utils/writer/icon_mapper_writer_impl.dart';
